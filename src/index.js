3225504ecd723151939ffb13034abc7049098dd1
Chekan-Liza <70697985+Chekan-Liza@users.noreply.github.com>
2022-07-14 13:47:11 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  var result = [];
+
+  for(let d in matrix){
+    if(d % 2 == 0){
+      matrix[d].sort(function(a, b) {
+        return a - b;
+      });
+      for(let w in matrix[d]){
+        result.push(matrix[d][w]);
+      }
+    } else {
+      matrix[d].sort(function(a, b) {
+        return b - a;
+      });
+      for(let w in matrix[d]){
+        result.push(matrix[d][w]);
+      }
+    }
+  }
+
+  return result;
 }
